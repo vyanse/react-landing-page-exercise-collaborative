8b8bd8da96d95e983d0deb86caadeccbe460ec9b
Katheren Salom <katherensalom@gmail.com>
2022-06-24 21:11:38 +0000

    axctualizacion del archivo

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,28 +2,10 @@ import React from "react";
 
 const Footers = () => {
     return (
-        /*<div className="text-center p-3">
-            Copyright © Your Website 2019
-        </div>*/
-        /*<nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
-            <p className="d-flex justify-content-center navbar-brand">Copyright © Your Website 2019</p>
-
-        </nav>*/
-        
-        /*<div className="container-fluid"> 
-            <div className="row">
-                <div className="col-sm-12 d-flex justify-content-center">
-                        <h6 className="text-center p-3 mb-2 text-white bg-dark">Copyright © Your Website 2019</h6>.
-                </div>
-            </div>
-        </div>*/
-        /*Copyright*/
         <div className="text-center p-3">
-            <h6 className="text-white">Copyright © Your Website 2019</h6>
+            <p className="text-white">Copyright © Your Website 2019</p>
            
-        </div>
-        /* Copyright */
-        
+        </div>        
     );
 }
 
