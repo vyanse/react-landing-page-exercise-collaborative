ec314d1430a2a5cf33fbdb5c4ea8b62b57e13b24
Katheren Salom <katherensalom@gmail.com>
2022-06-24 16:00:19 +0000

    se actualizo el codigo navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 
+const data = {
+	navbarTitle: "Start Bootstrap",
+	navbarHome: "Home",
+	navbarAbout: "About",
+    navbarServices: "Services",
+    navbarContact: "Contact"
+};
+
 //create your first component
 const Nabvar = () => {
 	return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <a className="navbar-brand" href="#">{data.navbarTitle}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div id="navbarCollapse" className="collapse navbar-collapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="#">{data.navbarHome}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">About</a>
+                            <a className="nav-link active" aria-current="page" href="#">{data.navbarAbout}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Services</a>
+                            <a className="nav-link active" aria-current="page" href="#">{data.navbarServices}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Contact</a>
+                            <a className="nav-link active" aria-current="page" href="#">{data.navbarContact}</a>
                         </li>
                     </ul>
                 </div>
