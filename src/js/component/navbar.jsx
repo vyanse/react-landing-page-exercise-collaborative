0dc9d6c5b40f5525ba9eac33187acd731075e63e
Katheren Salom <katherensalom@gmail.com>
2022-06-24 15:29:31 +0000

    actualizacion de codigo

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,11 +3,11 @@ import React from "react";
 //create your first component
 const Nabvar = () => {
 	return (
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div id="navbarCollapse" className="collapse navbar-collapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
